240c3fb60271e05d3b01fab9c80c3b239d9044f0
nafifurqon <muhammadnafi98@gmail.com>
2022-07-16 12:12:43 +0700

    32. Solution: Big O Calculation

---
+++ big_o/funChallange.js
@@ -0,0 +1,14 @@
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) { // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+// 3 steps + n + n + n + n
+// Big O Notation = O(3 + 4n) => O(n)
