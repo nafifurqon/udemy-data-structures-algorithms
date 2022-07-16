8e43f2e55c4f23a56105376b43dfdfbfd01800c1
nafifurqon <muhammadnafi98@gmail.com>
2022-07-16 11:55:52 +0700

    30. O(1)

--- big_o/findNemo.js
+++ big_o/findNemo.js
@@ -15,4 +15,13 @@ function findNemo(array) {
 
 // findNemo(nemo);
 // findNemo(everyone);
-findNemo(large);
+// findNemo(large);
+
+const boxes = [0,1,2,3,4,5]
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0])
+  console.log(boxes[1])
+}
+
+logFirstTwoBoxes(boxes)
