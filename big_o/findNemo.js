1718d329d44a82bfe8eeaea8b016249dcf521b02
nafifurqon <muhammadnafi98@gmail.com>
2022-07-18 11:41:56 +0700

    36. Big O Rule 1

--- big_o/findNemo.js
+++ big_o/findNemo.js
@@ -1,20 +1,22 @@
 const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'squirt', 'darla', 'hank']
+const everyone = ['dory', 'bruce', 'marlin', 'gill', 'bloat', 'squirt', 'darla', 'hank', 'nemo']
 const large = new Array(100000).fill('nemo');
 
 function findNemo(array) {
-  const t0 = performance.now();
+  // const t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
+    console.log('running')
     if (array[i] === 'nemo') {
       console.log('found NEMO!')
+      break;
     }
   }
-  const t1 = performance.now()
-  console.log(`Call to find nemo took ${t1 - t0} milliseconds`)
+  // const t1 = performance.now()
+  // console.log(`Call to find nemo took ${t1 - t0} milliseconds`)
 }
 
 // findNemo(nemo);
-// findNemo(everyone);
+findNemo(everyone);
 // findNemo(large);
 
 const boxes = [0,1,2,3,4,5]
