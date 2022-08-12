4b1ac8671b5453d25386453e86eaa40fdf146d79
nafifurqon <muhammadnafi98@gmail.com>
2022-08-12 17:40:22 +0700

    65. Static vs Dynamic Arrays

--- 1_data_structure_array/array.js
+++ 1_data_structure_array/array.js
@@ -21,3 +21,11 @@ console.log('strings', strings)
 // splice
 strings.splice(2, 0, 'alien') // O(n)
 console.log('strings', strings)
+
+// in C++
+// int a[20]; -> allocate memory for static array
+
+// static array akan selalu menggunakan alamat memory yang sama
+// karena sudah dialokasikan
+
+// dynamic array kadang meng-copy ke alamat memory yang baru
