a7928fbd79cc8a3702254cc157efc88cd1ce0a8c
nafifurqon <muhammadnafi98@gmail.com>
2022-08-14 17:53:03 +0700

    81. Solution: Implement A Hash Table

--- 2_data_structure_hash_tables/hash_table.js
+++ 2_data_structure_hash_tables/hash_table.js
@@ -12,16 +12,29 @@ class HashTable {
   }
 
   set(key, value) {
-    const index = this._hash(key.toString());
-    this.data[index] = [key, value];
-  }
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  } // O(1)
 
   get(key) {
-    const index = this._hash(key.toString());
-    return this.data[index][1];
-  }
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket.length) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  } // O(1) -> jika tanpa collision. Tapi, pada kenyatannya memang seringkali O(1)
 }
 
 const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000);
-console.log(myHashTable.get('grapes'))
+console.log(myHashTable.set("grapes", 10000));
+console.log(myHashTable.set("apples", 54));
+console.log(myHashTable.get("grapes"));
