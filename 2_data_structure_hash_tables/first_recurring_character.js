463da174b12a769344a2ad9735e1dbd685a99905
nafifurqon <muhammadnafi98@gmail.com>
2022-08-14 18:17:00 +0700

    85. Exercise: First Recurring Character (my solution)

--- 2_data_structure_hash_tables/first_recurring_character.js
+++ 2_data_structure_hash_tables/first_recurring_character.js
@@ -7,3 +7,25 @@
 
 // Given an array = [2,3,4,5]
 // It should return undefined
+
+function firstRecurringCharacter(array) {
+  if (array.length < 2) {
+    return undefined;
+  }
+
+  const hash = {};
+  for (let i = 0; i < array.length; i++) {
+    if (!hash[array[i]]) {
+      hash[array[i]] = 1;
+    } else {
+      return array[i]
+    }
+  }
+  return undefined;
+}
+
+const array = [2,5,1,2,3,5,1,2,4]
+// const array = [2,1,1,2,3,5,1,2,4]
+// const array = [2,3,4,5]
+
+console.log(firstRecurringCharacter(array))
