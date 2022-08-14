face1044ae8ef64b561215cbb1b20c42c040fa07
nafifurqon <muhammadnafi98@gmail.com>
2022-08-14 18:37:01 +0700

    86. Solution: First Recurring Character

--- 2_data_structure_hash_tables/first_recurring_character.js
+++ 2_data_structure_hash_tables/first_recurring_character.js
@@ -8,24 +8,60 @@
 // Given an array = [2,3,4,5]
 // It should return undefined
 
-function firstRecurringCharacter(array) {
-  if (array.length < 2) {
-    return undefined;
-  }
+// my solution
+// function firstRecurringCharacter(array) {
+//   if (array.length < 2) {
+//     return undefined;
+//   }
 
-  const hash = {};
-  for (let i = 0; i < array.length; i++) {
-    if (!hash[array[i]]) {
-      hash[array[i]] = 1;
-    } else {
-      return array[i]
+//   const hash = {};
+//   for (let i = 0; i < array.length; i++) {
+//     if (!hash[array[i]]) {
+//       hash[array[i]] = 1;
+//     } else {
+//       return array[i]
+//     }
+//   }
+//   return undefined;
+// }
+
+// const array = [2,5,1,2,3,5,1,2,4]
+// const array = [2,1,1,2,3,5,1,2,4]
+// const array = [2,3,4,5]
+
+// console.log(firstRecurringCharacter(array))
+
+// Andrei solution
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
     }
   }
   return undefined;
-}
+} // time complexity = O(n^2), space complexity = O(1)
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined;
+} // time complexity = O(n), space complexisty = O(n)
 
 const array = [2,5,1,2,3,5,1,2,4]
 // const array = [2,1,1,2,3,5,1,2,4]
 // const array = [2,3,4,5]
 
+const array1 = [2,5,5,1,2,3,5,1,2,4]
+
 console.log(firstRecurringCharacter(array))
+console.log(firstRecurringCharacter(array1)) //  case array1, return 2 -> false
+console.log(firstRecurringCharacter2(array))
+console.log(firstRecurringCharacter2(array1)) //  case array1, return 5 -> true
