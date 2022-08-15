7fb53074b0ca06629806da55472990b4d3dab78b
nafifurqon <muhammadnafi98@gmail.com>
2022-08-15 14:53:09 +0700

    99. insert()

--- 3_data_structure_linked_list/linked_list.js
+++ 3_data_structure_linked_list/linked_list.js
@@ -1,5 +1,6 @@
 // 10 -> 5 -> 16
-// 1->10 -> 5 -> 16
+// 1 -> 10 -> 5 -> 16 //prepend(value)
+// 1 -> 10 -> 99 -> 5 -> 16 //insert(index, value)
 
 // let myLinkedList = {
 //   head: {
@@ -46,16 +47,33 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-console.log('myLinkedList', myLinkedList)
+console.log('myLinkedList', myLinkedList.printList())
 
 myLinkedList.append(5);
-console.log('myLinkedList', myLinkedList)
+console.log('myLinkedList', myLinkedList.printList())
 
 myLinkedList.append(16);
-console.log('myLinkedList', JSON.stringify(myLinkedList, null, 4))
+console.log('myLinkedList', myLinkedList.printList())
 
 myLinkedList.prepend(1);
-console.log('myLinkedList', JSON.stringify(myLinkedList, null, 4))
+console.log('myLinkedList', myLinkedList.printList())
+
+// myLinkedList.insert(2, 99);
+// console.log('myLinkedList', myLinkedList.printList())
