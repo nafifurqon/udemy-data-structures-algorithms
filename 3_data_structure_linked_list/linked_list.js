3c379e104cbf4d85bd064a2e359cef60ad6b5a58
nafifurqon <muhammadnafi98@gmail.com>
2022-08-15 14:28:30 +0700

    95. Our First Linked List

---
+++ 3_data_structure_linked_list/linked_list.js
@@ -0,0 +1,34 @@
+// 10 -> 5 -> 16
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null
+//       }
+//     }
+//   }
+// }
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    // Code here
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+console.log('myLinkedList', myLinkedList)
+
+myLinkedList.append(5);
