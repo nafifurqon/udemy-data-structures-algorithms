6000e3afca57efd589187f25e7bfa991604dbe92
nafifurqon <muhammadnafi98@gmail.com>
2022-08-15 14:44:13 +0700

    97. Solution: prepend()

--- 3_data_structure_linked_list/linked_list.js
+++ 3_data_structure_linked_list/linked_list.js
@@ -38,8 +38,9 @@ class LinkedList {
   prepend(value) {
     const newNode = {
       value: value,
-      next: this.head,
+      next: null,
     }
+    newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
