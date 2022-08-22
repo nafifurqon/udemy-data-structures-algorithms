ded510fe2078297a4d2f3fe852228a85ceece62b
nafifurqon <muhammadnafi98@gmail.com>
2022-08-22 20:50:54 +0700

    118. Solution: Stack Implementation (Linked Lists)

--- 4_data_structure_stack/stack-linkedlist.js
+++ 4_data_structure_stack/stack-linkedlist.js
@@ -11,13 +11,54 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
-  peek() {}
-  push(value) {}
-  pop() {}
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+
+    // const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    // return holdingPointer;
+    return this;
+  }
   //isEmpty
 }
 
 const myStack = new Stack();
+
+console.log('myStack.push("Google")', myStack.push("Google"));
+console.log('myStack.push("Udemy")', myStack.push("Udemy"));
+console.log('myStack.push("Discord")', myStack.push("Discord"));
+console.log(myStack.peek());
+console.log('myStack.pop()', myStack.pop());
+console.log('myStack.pop()', myStack.pop());
+console.log('myStack.pop()', myStack.pop());
 
 //Discord
 //Udemy
