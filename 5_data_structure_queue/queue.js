82d750c2be72f0be7c40259009516ec7f7fb528e
nafifurqon <muhammadnafi98@gmail.com>
2022-08-24 21:01:22 +0700

    121. Exercise: Queue Implementation

---
+++ 5_data_structure_queue/queue.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+		return this.first;
+  }
+
+  enqueue(value){
+		const newNode = new Node(value);
+
+		if (this.length === 0) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			const holdingPointer = this.last;
+			this.last = newNode;
+			holdingPointer.next = newNode;
+		}
+
+		this.length++;
+		return this;
+  }
+
+  dequeue(){
+		if (!this.first) {
+      return null;
+    }
+
+		if (this.first === this.last) {
+			this.last = null;
+		}
+
+		this.first = this.first.next;
+		this.length--;
+		return this;
+  }
+
+  isEmpty() {
+		return this.length === 0 ? true : false;
+	}
+}
+
+const myQueue = new Queue();
+
+console.log('myQueue.peek()', myQueue.peek())
+console.log('myQueue.enqueue("Joy")', myQueue.enqueue("Joy"))
+console.log('myQueue.enqueue("Matt")', myQueue.enqueue("Matt"))
+console.log('myQueue.enqueue("Pavel")', myQueue.enqueue("Pavel"))
+console.log('myQueue.dequeue()', myQueue.dequeue())
+console.log('myQueue.dequeue()', myQueue.dequeue())
+console.log('myQueue.dequeue()', myQueue.dequeue())
+
+//Joy
+//Matt
+//Pavel
+//Samir
